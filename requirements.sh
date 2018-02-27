0e3e9f35cdd0dc8d3d4ad0ecc0f05cd86f3143a9
Giovanni Colavizza <gcolavizza@turing.ac.uk>
2018-02-27 14:05:10 +0000

    Add files via upload

---
+++ requirements.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+echo "Getting ready to install Foo"
+#export PYSPARK_PYTHON=python3
+#export PYSPARK_DRIVER_PYTHON=python3
+#apt-get install python3-pip
+#pip3 install numpy boto boto3 botocore warcio numpy dynamic-pybloom
