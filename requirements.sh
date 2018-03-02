4a7bee852b85ce9ab94bd66765bb512627fc3195
Giovanni Colavizza <gcolavizza@turing.ac.uk>
2018-03-02 14:24:43 +0000

    Delete requirements.sh

--- requirements.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-echo "Getting ready to install stuff"
-pip install --upgrade pip  | tee echo
-pip install numpy boto boto3 botocore warcio pybloom  | tee echo
-export PYSPARK_PYTHON=python
-export PYSPARK_DRIVER_PYTHON=python
-echo "All done"
