c09f864f08b902da3ace401792b3484e0df6ea6a
Giovanni Colavizza <gcolavizza@turing.ac.uk>
2018-03-02 14:37:47 +0000

    Add files via upload

---
+++ requirements.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+echo "Getting ready to install stuff"
+pip install --upgrade pip  | tee echo
+pip uninstall -y warcio  | tee echo
+pip install six  | tee echo
+pip install numpy boto boto3 botocore warcio pybloom  | tee echo
+export PYSPARK_PYTHON=python
+export PYSPARK_DRIVER_PYTHON=python
+echo "All done"
