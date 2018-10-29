eef5b883635586d0aec761bf7f40ff0d1a08aac8
Giovanni Colavizza <gcolavizza@turing.ac.uk>
2018-10-29 12:32:13 +0000

    added pandas

--- requirements.sh
+++ requirements.sh
@@ -2,7 +2,7 @@
 
 echo "Getting ready to install stuff"
 pip install --upgrade pip  | tee echo
-pip install --upgrade --force-reinstall configparser numpy boto boto3 botocore warcio pybloom | tee echo
+pip install --upgrade --force-reinstall configparser numpy boto boto3 botocore warcio pybloom pandas | tee echo
 export PYSPARK_PYTHON=python
 export PYSPARK_DRIVER_PYTHON=python
 echo "All done"
