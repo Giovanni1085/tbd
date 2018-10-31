cf4dd4255076f26730f01b6f636ec3ff4c4e2425
Giovanni Colavizza <gcolavizza@turing.ac.uk>
2018-10-31 10:37:36 +0000

    Seaborn

--- requirements.sh
+++ requirements.sh
@@ -2,9 +2,9 @@
 
 echo "Getting ready to install stuff"
 python -m pip install --upgrade pip  | tee echo
-pip install --upgrade --force-reinstall configparser numpy boto boto3 botocore warcio pybloom pandas beautifulsoup4 | tee echo
+pip install --upgrade --force-reinstall configparser numpy boto boto3 botocore warcio pybloom pandas beautifulsoup4 seaborn | tee echo
 sudo apt install python3-pip | tee echo
-pip3 inspall pandas | tee echo
+pip3 inspall seaborn pandas seaborn | tee echo
 export PYSPARK_PYTHON=python
 export PYSPARK_DRIVER_PYTHON=python
 echo "All done"
