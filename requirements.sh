a13c54d187f57b85c74c77d04325e1d4d71ac440
Giovanni Colavizza <gcolavizza@turing.ac.uk>
2018-10-31 10:33:00 +0000

    Adding some bits of Python3

--- requirements.sh
+++ requirements.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
 echo "Getting ready to install stuff"
-pip install --upgrade pip  | tee echo
+python -m pip install --upgrade pip  | tee echo
 pip install --upgrade --force-reinstall configparser numpy boto boto3 botocore warcio pybloom pandas beautifulsoup4 | tee echo
+sudo apt install python3-pip | tee echo
+pip3 inspall pandas | tee echo
 export PYSPARK_PYTHON=python
 export PYSPARK_DRIVER_PYTHON=python
 echo "All done"
