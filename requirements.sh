743603ab2301b6aca9167f0e0d2bc7801442880f
Giovanni Colavizza <gcolavizza@turing.ac.uk>
2018-10-31 16:47:53 +0000

    Align python 3

--- requirements.sh
+++ requirements.sh
@@ -4,7 +4,7 @@ echo "Getting ready to install stuff"
 python -m pip install --upgrade pip  | tee echo
 pip install --upgrade --force-reinstall configparser numpy boto boto3 botocore warcio pybloom pandas beautifulsoup4 seaborn matplotlib | tee echo
 sudo apt install python3-pip | tee echo
-pip3 inspall seaborn pandas seaborn matplotlib | tee echo
+pip3 install --upgrade --force-reinstall configparser numpy boto boto3 botocore warcio pybloom pandas beautifulsoup4 seaborn matplotlib | tee echo
 export PYSPARK_PYTHON=python
 export PYSPARK_DRIVER_PYTHON=python
 echo "All done"
