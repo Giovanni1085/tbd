18453b25ead571a1a5831ce939a74d8d1313088c
Giovanni Colavizza <gcolavizza@turing.ac.uk>
2018-11-22 10:35:33 +0000

    Removed py3

--- requirements.sh
+++ requirements.sh
@@ -3,8 +3,6 @@
 echo "Getting ready to install stuff"
 python -m pip install --upgrade pip  | tee echo
 pip install --upgrade --force-reinstall configparser numpy boto boto3 botocore warcio pybloom pandas beautifulsoup4 seaborn matplotlib | tee echo
-sudo apt install python3-pip | tee echo
-pip3 install --upgrade --force-reinstall configparser numpy boto boto3 botocore warcio pybloom pandas beautifulsoup4 seaborn matplotlib | tee echo
 export PYSPARK_PYTHON=python
 export PYSPARK_DRIVER_PYTHON=python
 echo "All done"
